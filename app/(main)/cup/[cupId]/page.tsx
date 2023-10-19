bf245b0bddaf0877a832048a0135c70783e34204
danpoj <jws970306@khu.ac.kr>
2023-10-19 14:59:36 +0900

    add: dynamic metadata

--- app/(main)/cup/[cupId]/page.tsx
+++ app/(main)/cup/[cupId]/page.tsx
@@ -9,6 +9,47 @@ type Props = {
   }
 }
 
+import { Metadata, ResolvingMetadata } from 'next'
+
+export async function generateMetadata(
+  { params: { cupId } }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata | undefined> {
+  const cup = await db.cup.findUnique({
+    where: {
+      id: cupId,
+    },
+  })
+
+  if (!cup) return
+
+  return {
+    title: `PingPing 이상형 월드컵 | ${cup.title}`,
+    description: cup.description,
+
+    openGraph: {
+      title: `PingPing 이상형 월드컵 | ${cup.title}`,
+      description: cup.description,
+      url: `https://www.pingping.online/cup/${cup.id}`,
+      siteName: 'PingPing',
+      locale: 'ko_KR',
+      type: 'website',
+      images: [
+        {
+          url: cup.thumbnail,
+        },
+      ],
+    },
+
+    twitter: {
+      card: 'summary_large_image',
+      title: `PingPing 이상형 월드컵 | ${cup.title}`,
+      description: cup.description,
+      images: [cup.thumbnail],
+    },
+  }
+}
+
 export default async function Page({ params: { cupId } }: Props) {
   const cup = await db.cup.findUnique({
     where: {
