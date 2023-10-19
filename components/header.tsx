1350c2a46aa6671515a0889df4c1a0ea5c27d871
danpoj <jws970306@khu.ac.kr>
2023-10-19 20:21:00 +0900

    style

--- components/header.tsx
+++ components/header.tsx
@@ -22,7 +22,7 @@ export default function Header({ session }: Props) {
 
   return (
     <header className='fixed w-full px-3 h-12 flex justify-between items-center bg-background/90 z-50 backdrop-blur-sm'>
-      <div className='flex items-center gap-4 w-full'>
+      <div className='flex items-center gap-4'>
         <PingpingLogo />
         <Button
           className='hidden sm:block text-xs font-bold h-8'
