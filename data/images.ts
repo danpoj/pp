3291bbf3a74f774cd289e314a3da2b4da66b1530
danpoj <jws970306@khu.ac.kr>
2023-10-23 11:54:32 +0900

    refactor: images to base64

--- data/images.ts
+++
@@ -1,6 +0,0 @@
-import img1 from 'public/create/image1.png'
-import img2 from 'public/create/image2.gif'
-import img3 from 'public/create/image3.jpeg'
-import img4 from 'public/create/image4.gif'
-
-export const images = [img1, img3, img2, img4]
