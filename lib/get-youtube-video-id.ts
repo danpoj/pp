9ca7d8fbd87d708839b3b5f83e427f8c915134fc
danpoj <jws970306@khu.ac.kr>
2023-10-24 20:38:58 +0900

    add: support youtube shorts

--- lib/get-youtube-video-id.ts
+++ lib/get-youtube-video-id.ts
@@ -2,7 +2,9 @@ export function getYouTubeVideoId(url: string | null) {
   if (!url) throw new Error('유효하지 않은 유튜브 링크입니다.')
   if (!url.startsWith('https://www.youtube.com')) throw new Error('유효하지 않은 유튜브 링크입니다.')
 
-  const urlObj = new URL(url)
+  const newUrl = url.includes('youtube.com/shorts') ? url.split('?')[0].replace('shorts/', 'watch?v=') : url
+
+  const urlObj = new URL(newUrl)
 
   const videoId = urlObj.searchParams.get('v')
 
