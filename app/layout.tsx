d4d40271631cca324cf09572e48c48e56c012b32
danpoj <jws970306@khu.ac.kr>
2023-11-09 13:28:20 +0900

    preffered region

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,6 @@ const notoSans = Noto_Sans({
   weight: ['500'],
 })
 
-export const preferredRegion = 'icn1'
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko' suppressHydrationWarning>
