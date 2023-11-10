d8be8435e2a92b23ad64663e657177abe51c279f
danpoj <jws970306@khu.ac.kr>
2023-11-10 15:34:54 +0900

    fix: layout screw

--- components/adsense/google-adsense.tsx
+++ components/adsense/google-adsense.tsx
@@ -18,11 +18,19 @@ export default function GoogleAdsense({ className }: { className?: string }) {
     <div className={cn('rounded-lg overflow-hidden shadow dark:bg-border/20 border w-full', className)}>
       <ins
         className='adsbygoogle block'
+        data-ad-format='fluid'
+        data-ad-layout-key='-7w+eo+0+2+1'
+        data-ad-client='ca-pub-3412419424653583'
+        data-ad-slot='6021221123'
+      />
+
+      {/* <ins
+        className='adsbygoogle block'
         data-ad-client='ca-pub-3412419424653583'
         data-ad-slot='5019393970'
         data-ad-format='auto'
         data-full-width-responsive='true'
-      ></ins>
+      ></ins> */}
     </div>
   )
 }
