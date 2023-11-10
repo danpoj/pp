4257ea9ad3c9ecc1ddc4dfb8802a721b909ee254
danpoj <jws970306@khu.ac.kr>
2023-11-10 12:16:55 +0900

    google adsense test

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         async
         crossOrigin='anonymous'
         src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3412419424653583'
-        strategy='lazyOnload'
+        // strategy='lazyOnload'
       />
 
       <body className={cn('antialiased text-sm', notoSans.className)}>
---
+++ components/adsense/google-adsense.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useEffect } from 'react'
+
+declare global {
+  interface Window {
+    adsbygoogle: any
+  }
+}
+
+export default function GoogleAdsense() {
+  useEffect(() => {
+    ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+  }, [])
+
+  return (
+    <div className='rounded-lg overflow-hidden shadow dark:bg-border/20 border w-full'>
+      <ins
+        className='adsbygoogle block'
+        data-ad-format='fluid'
+        data-ad-layout-key='-7w+eo+0+2+1'
+        data-ad-client='ca-pub-3412419424653583'
+        data-ad-slot='6021221123'
+      />
+    </div>
+  )
+}
