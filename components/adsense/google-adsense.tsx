b6be50e5653ad5f98e4f89a296d9d23d39616f91
danpoj <jws970306@khu.ac.kr>
2023-11-10 13:56:42 +0900

    google adsense

--- components/adsense/google-adsense.tsx
+++ components/adsense/google-adsense.tsx
@@ -16,13 +16,21 @@ export default function GoogleAdsense({ className }: { className?: string }) {
 
   return (
     <div className={cn('rounded-lg overflow-hidden shadow dark:bg-border/20 border w-full', className)}>
-      <ins
+      {/* <ins
         className='adsbygoogle block'
         data-ad-format='fluid'
         data-ad-layout-key='-7w+eo+0+2+1'
         data-ad-client='ca-pub-3412419424653583'
         data-ad-slot='6021221123'
-      />
+      /> */}
+
+      <ins
+        className='adsbygoogle block'
+        data-ad-client='ca-pub-3412419424653583'
+        data-ad-slot='5019393970'
+        data-ad-format='auto'
+        data-full-width-responsive='true'
+      ></ins>
     </div>
   )
 }
