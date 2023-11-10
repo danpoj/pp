3a5ba23f4484b1ca89715af624727aa76b5da51c
danpoj <jws970306@khu.ac.kr>
2023-11-10 15:20:06 +0900

    fix: layout shift - deploy

--- components/adsense/google-adsense.tsx
+++ components/adsense/google-adsense.tsx
@@ -16,14 +16,6 @@ export default function GoogleAdsense({ className }: { className?: string }) {
 
   return (
     <div className={cn('rounded-lg overflow-hidden shadow dark:bg-border/20 border w-full', className)}>
-      {/* <ins
-        className='adsbygoogle block'
-        data-ad-format='fluid'
-        data-ad-layout-key='-7w+eo+0+2+1'
-        data-ad-client='ca-pub-3412419424653583'
-        data-ad-slot='6021221123'
-      /> */}
-
       <ins
         className='adsbygoogle block'
         data-ad-client='ca-pub-3412419424653583'
