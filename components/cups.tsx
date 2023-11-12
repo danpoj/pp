acc5f56caffbfcc3f47f470aa238134abda113a8
danpoj <jws970306@khu.ac.kr>
2023-11-12 15:35:24 +0900

    style

--- components/cups.tsx
+++ components/cups.tsx
@@ -64,7 +64,7 @@ export default function Cups({ count, initialCups, session, isLiked = false, typ
 
   return (
     <section className='sm:px-2 pb-20'>
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6'>
         {cups.map((cup, index) => {
           if (index % 2 === 0) {
             return (
