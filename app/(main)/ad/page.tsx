9a71a1d8093c9f4c3be4c24184343d7e27f21100
danpoj <jws970306@khu.ac.kr>
2023-11-14 16:48:05 +0900

    test

--- app/(main)/ad/page.tsx
+++ app/(main)/ad/page.tsx
@@ -3,7 +3,7 @@ import GoogleAdsense from '@/components/adsense/google-adsense'
 export default function Page() {
   return (
     <div className='w-full h-full grid sm:grid-cols-2 xl:grid-cols-3'>
-      {new Array(100).fill(undefined).map((_, i) => (
+      {new Array(12).fill(undefined).map((_, i) => (
         <GoogleAdsense key={i} className='border h-32' />
       ))}
     </div>
