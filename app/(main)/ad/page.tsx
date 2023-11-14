7447af9c7ac12b586460dfefe4d212144d3bce49
danpoj <jws970306@khu.ac.kr>
2023-11-14 17:13:12 +0900

    test

--- app/(main)/ad/page.tsx
+++ app/(main)/ad/page.tsx
@@ -1,11 +1,21 @@
+'use client'
+
 import GoogleAdsense from '@/components/adsense/google-adsense'
+import GoogleAdsenseHorizontal from '@/components/adsense/google-adsense-horizontal'
+import GoogleAdsenseVertical from '@/components/adsense/google-adsense-vertical'
 
 export default function Page() {
   return (
-    <div className='w-full h-full grid sm:grid-cols-2 xl:grid-cols-3'>
-      {new Array(12).fill(undefined).map((_, i) => (
-        <GoogleAdsense key={i} className='border h-32' />
+    <div className='w-full h-full flex flex-wrap'>
+      {new Array(4).fill(undefined).map((_, i) => (
+        <GoogleAdsense key={i} className='border aspect-square max-w-[30rem]' />
       ))}
+
+      <GoogleAdsenseHorizontal className='border aspect-square max-w-[42rem]' />
+      <GoogleAdsenseVertical className='border aspect-square max-w-[42rem]' />
+      <GoogleAdsenseHorizontal className='border aspect-square max-w-[42rem]' />
+
+      <GoogleAdsenseVertical className='border aspect-square max-w-[42rem]' />
     </div>
   )
 }
